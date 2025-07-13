8baea741c044d4e1e68ea6740b982527e6cf97fb
Yogesh <147805708+yogeshMern@users.noreply.github.com>
2025-07-13 21:05:56 +0530

    Update connect.js

--- back/db/connect.js
+++ back/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "");
+    const conn = await mongoose.connect("mongodb://localhost:27017/project_management" || "");
 
     console.log(
       `MongoDB Connected: ${conn.connection.host}:${conn.connection.port}`
