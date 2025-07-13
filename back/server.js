d3bccb236bd33306a794b2b56283fc3dbe60d6b2
Yogesh <147805708+yogeshMern@users.noreply.github.com>
2025-07-13 20:45:08 +0530

    Update server.js

--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ const connectDB = require("./db/connect");
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
